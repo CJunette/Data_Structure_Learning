c6c9272f27a0225d0697faad2ed81d9f9ba2d489
fengshuochange <fengshuochange@gmail.com>
2020-08-12 11:39:04 +0800

    Changes to Range

--- PA1/Range/Range/Range.cpp
+++ PA1/Range/Range/Range.cpp
@@ -2,8 +2,6 @@
 //
 
 #include <iostream>
-//#include <ctime>
-using namespace std;
 
 int inline searchlo(int *s, int s_size, int a)
 {
@@ -78,16 +76,10 @@ void inline sort(int *s, int lo, int hi)
 
 int main()
 {
-	//timespec start, end;
-
     int n, m;
 	scanf("%d%d", &n, &m);
 	//用scanf和printf替换cin和cout，可以节省大量时间。
 	int s[500000];
-	//timespec_get(&start, 1);
-	//timespec_get(&end, 1);
-	//int time = (end.tv_sec - start.tv_sec) + 0.000000001 * (end.tv_nsec - start.tv_nsec);
-	//cout << "time1: " << time << endl;
 
 	for(int i = 0; i < n; i++)
 	{
@@ -95,12 +87,8 @@ int main()
 	}
 	sort(s, 0, n);
 
-	//timespec_get(&start, 1);
 	int a[500000];
 	int b[500000];
-	//timespec_get(&end, 1);
-	//time = (end.tv_sec - start.tv_sec) + 0.000000001 * (end.tv_nsec - start.tv_nsec);
-	//cout << "time2: " << time << endl;
 	
 	int i = 0;
 	while(i < m)
@@ -111,18 +99,13 @@ int main()
 
 	i = 0;
 	int count;
-	//timespec_get(&start, 1);
+
 	while(i < m)
 	{
-
 		int lo = searchlo(s, n, a[i]);
 		int hi = searchhi(s, n, b[i]);
 		count = hi - lo;
 		printf("%d\n", count);
 		i++;
 	}
-	//timespec_get(&end, 1);
-	//time = (end.tv_sec - start.tv_sec) + 0.000000001 * (end.tv_nsec - start.tv_nsec);
-	//cout << "time3: " << time << endl;
-
 }
